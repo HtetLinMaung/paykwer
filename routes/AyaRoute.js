1d32fe954b99e2ba9d580fd982592d84cdbba38c
Htet Lin Maung <jsthtet96@gmail.com>
2020-07-18 15:21:43 +0630

    update

--- routes/AyaRoute.js
+++ routes/AyaRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const qs = require("qs");
+const { v4 } = require("uuid");
 const router = express.Router();
 
 router.post("/loginAYA", (req, res) => {
@@ -21,6 +22,7 @@ router.post("/loginAYA", (req, res) => {
       }
     )
     .then((response) => {
+      console.log(response);
       access_token = response.data.access_token;
       axios
         .post(
@@ -36,11 +38,62 @@ router.post("/loginAYA", (req, res) => {
           }
         )
         .then((response2) => {
+          console.log(response2);
           user_token = response2.data.token.token;
           res.json({ access_token, user_token });
         });
     })
     .catch((err) => console.log(err));
+});
+
+router.post("/enquiryTransaction", (req, res) => {
+  console.log(req.body.user_token);
+  console.log(req.body.access_token);
+  console.log("under");
+  console.log({
+    serviceCode: "AGENTTRANSFERTONRIC",
+    depositorPhone: req.body.depositorPhone,
+    depositorName: req.body.depositorName,
+    beneficiaryNric: req.body.beneficiaryNric,
+    beneficiaryPhone: req.body.beneficiaryPhone,
+    beneficiaryName: req.body.beneficiaryName,
+    amount: req.body.amount,
+    externalTransactionId: `${v4()}`,
+    externalAdditionalData: req.body.externalAdditionalData,
+    message: req.body.message,
+    currency: req.body.currency
+  });
+  axios
+    .post(
+      "https://opensandbox.ayainnovation.com/agent/1.0.0/thirdparty/agent/enquiryTransaction",
+      qs.stringify({
+        serviceCode: "AGENTTRANSFERTONRIC",
+        depositorPhone: req.body.depositorPhone,
+        depositorName: req.body.depositorName,
+        beneficiaryNric: req.body.beneficiaryNric,
+        beneficiaryPhone: req.body.beneficiaryPhone,
+        beneficiaryName: req.body.beneficiaryName,
+        amount: req.body.amount,
+        externalTransactionId: `${v4()}`,
+        externalAdditionalData: req.body.externalAdditionalData,
+        message: req.body.message,
+        currency: req.body.currency
+      }),
+      {
+        headers: {
+          Authorization: `Bearer ${req.body.user_token}`,
+          Token: `Bearer ${req.body.access_token}`,
+          "Accept-Language": "en",
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      }
+    )
+    .then((response) => {
+      res.json(response.data.data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
