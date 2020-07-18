7c1d146bbc1069fa36d43824c890df4140a7aaed
Yin2792 <yinyin2792@gmail.com>
2020-07-18 10:24:29 +0630

    update

---
+++ routes/AuthRoute.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router();
+const GET_AYA__TOKEN_API= ""
+
+//AYAAPIGET
+router.post(GET_AYA__TOKEN_API,(req,res)=>{
+
+    console.log(req)
+})
